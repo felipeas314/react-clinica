6cdb0f7019e638a1d7627d60c116aa1187e73c28
Felipe <felipeas314@gmail.com>
2020-08-06 23:51:34 -0300

    primeiro commit

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch } from 'react-router-dom';
+import Route from './Route';
+
+import Dashboard from '../pages/Dasboard';
+import Profile from '../pages/Profile';
+import SignIng from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+export default function Routes(){
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIng}/>
+      <Route path="/register" component={SignUp}/>
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
